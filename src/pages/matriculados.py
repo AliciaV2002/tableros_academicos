07acb7e173f54a20faa2c722163b61bb64f95c00
AliciaV2002 <113649609+AliciaV2002@users.noreply.github.com>
2025-04-01 09:13:30 -0500

    agregando requirements

--- src/pages/matriculados.py
+++ src/pages/matriculados.py
@@ -4,7 +4,6 @@ import pandas as pd
 import numpy as np
 from dplython import *
 import matplotlib.pyplot as plt
-import seaborn as sns
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
